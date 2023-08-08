866caf3bb9641a44a72993db3b4d7e10af846463
BrayanDesarrollo6 <desarrollo6@hq5.com.co>
2023-08-08 09:25:30 -0500

    Correcciones de generacion de json 2

--- src/python/obtenerEmpresas.py
+++ src/python/obtenerEmpresas.py
@@ -23,9 +23,11 @@ if(df1.empty == False):
     File_Json = {}
     Empresas = df1['Empresa Usuaria'].unique().tolist()
     Empresas = [str(x) for x in Empresas]
+    Empresas = [x for x in Empresas if x != 'nan']
     File_Json["Empresas"] = Empresas
     Estados = df1['Estado'].unique().tolist()
     Estados = [str(x) for x in Estados]
+    Estados = [x for x in Estados if x != 'nan']
     File_Json["Estados"] = Estados
     if estado == "0":
         with open(DirectoryObtenerEmpresasLQ,"w") as file:

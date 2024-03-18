daba0085a3da5be509263e9275bcadffcf5a88be
SERGIO <desarrollo4@hq5.com.co>
2024-03-18 16:47:29 -0500

    Ajuste: generar reporte para todos los clientes

--- src/python/reporteFacturacionExamenes.py
+++ src/python/reporteFacturacionExamenes.py
@@ -8,14 +8,19 @@ import openpyxl
 def get_df(data):
     date_from = data["fecha_desde"]
     date_to = data["fecha_hasta"]
-    clientes = data["clientes"]
-    clientes = ','.join(clientes).replace(' ','+')
     
-    clientes_param =  "lp_examenes_programados.EMPRESA_USUARIA="+"["+clientes+"]"
     op_param ="lp_examenes_programados.FECHA_A_REALIZAR_EL_EXAMEN_op=58"
     date_param = "lp_examenes_programados.FECHA_A_REALIZAR_EL_EXAMEN="+date_from+";"+date_to
     
-    url_params = '&'.join([date_param,op_param,clientes_param])
+    params = [date_param,op_param]
+    
+    if('clientes' in data):
+        clientes = data["clientes"]
+        clientes = ','.join(clientes).replace(' ','+')
+        clientes_param =  "lp_examenes_programados.EMPRESA_USUARIA="+"["+clientes+"]"
+        params.append(clientes_param)
+    
+    url_params = '&'.join(params)
     
     url = "https://creatorapp.zohopublic.com/hq5colombia/hq5/xls/Profesiograma_Programacion_individual_Report/FjtXFPONhKfDuhOagEY0qMKWfRnMRXXu9Z3W1mWdmQZwNF1Qq1qHRhfA82YVT9DFPZ2S9RWHp8EsuKgEC9Q8r1WShRZ5zqYwb7C7?"
     url = url + url_params
